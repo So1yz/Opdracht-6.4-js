a4f547dbf915a957dbaff9adb9c75a81e4c0c80a
Yusuf <yusuf.simsek9080@gmail.com>
2025-11-10 10:24:54 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,24 @@
+function calculate(nummer1, nummer2)
+{
+    let resultaat = nummer1 + nummer2;
+    console.log(resultaat);
+}
+
+calculate(5, 10);
+
+
+function calculate2(nummer3)
+{
+    let resultaat2 = nummer3 * 5;
+    console.log(resultaat2);
+}
+
+calculate2(4);
+
+function calculateminutes(seconden)
+{
+    let minute = seconden / 60;
+    console.log(minute);
+}
+
+calculateminutes(3600);
